8a1b54bc182356424474abff9694fe9f06ff14f2
Ahmed Abdelnaser <ahmedabdelnaser770@gmail.com>
2026-01-20 01:15:42 +0200

    cart-Items-Style

--- Client/src/app/features/cart/cart.component.ts
+++ Client/src/app/features/cart/cart.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../../core/services/cart.service';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+  private router = inject(Router);
+  cartService = inject(CartService);
 
+  onAction() {
+    this.router.navigateByUrl('/shop');
+  }
 }
