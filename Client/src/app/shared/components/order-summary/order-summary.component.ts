a3d540bc1e629bc4c68b4e4ad99e73ca4eb2bbf6
Ahmed Abdelnaser <ahmedabdelnaser770@gmail.com>
2026-01-20 22:52:50 +0200

    catService&Components

--- Client/src/app/shared/components/order-summary/order-summary.component.ts
+++ Client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+import { CartService } from '../../../core/services/cart.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatButton,
+    RouterLink,
+    MatInput,
+    CurrencyPipe,
+    FormsModule,
+    MatIcon,
+  ],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss',
 })
 export class OrderSummaryComponent {
+  cartService = inject(CartService);
+  // private stripeService = inject(StripeService);
+  location = inject(Location);
+  code?: string;
 
+  applyCouponCode() {
+    if (!this.code) return;
+    this.cartService.applyDiscount(this.code).subscribe({
+      next: async (coupon) => {
+        const cart = this.cartService.cart();
+        if (cart) {
+          cart.coupon = coupon;
+          await firstValueFrom(this.cartService.setCart(cart));
+          this.code = undefined;
+        }
+        if (this.location.path() === '/checkout') {
+          // await firstValueFrom(this.stripeService.createOrUpdatePaymentIntent());
+        }
+      },
+    });
+  }
+
+  async removeCouponCode() {
+    const cart = this.cartService.cart();
+    if (!cart) return;
+    if (cart.coupon) cart.coupon = undefined;
+    await firstValueFrom(this.cartService.setCart(cart));
+    if (this.location.path() === '/checkout') {
+      // await firstValueFrom(this.stripeService.createOrUpdatePaymentIntent());
+    }
+  }
 }
